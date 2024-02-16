36bd7e247f7e18058b70feb0557444f8f3280a88
Adams, Alexander <adamsa@frib.msu.edu>
2024-02-16 10:56:21 -0500

    Made script which remaps pad numbers in an h5 file based on channel information

---
+++ raw_viewer/h5_remap.py
@@ -0,0 +1,32 @@
+import shutil
+
+import h5py
+import numpy as np
+import tqdm
+
+
+def remap_pad_numbers(input_file, output_file, flatlookup_file):
+    '''
+    Copy input file to output file, and then renumber the pads based on flatlookup_file
+    '''
+    flat_lookup = np.loadtxt(flatlookup_file, delimiter=',', dtype=int)
+    chnls_to_pad = {} #maps tuples of (asad, aget, channel) to pad number
+    for line in flat_lookup:
+        chnls = tuple(line[0:4])
+        pad = line[4]
+        chnls_to_pad[chnls] = pad
+    
+    shutil.copyfile(input_file, output_file)
+    with h5py.File(output_file, 'r+') as file:
+        first_event_num, last_event_num = int(file['meta']['meta'][0]), int(file['meta']['meta'][2])
+        for event_num in tqdm.tqdm(range(first_event_num, last_event_num+1)):
+            data = file['get']['evt%d_data'%event_num]
+            for line in data:
+                chnl_info = tuple(line[0:4])
+                if chnl_info in chnls_to_pad:
+                    data[4] = chnls_to_pad[chnl_info]
+
+if __name__ == '__main__':
+    input_file = '/mnt/analysis/e21072/gastest_h5_files/run_0099.h5'
+    output_file = '/mnt/analysis/e21072/h5test/gastest_run_0099_pads_remapped.h5'
+    remap_pad_numbers(input_file, output_file, 'flatlookup2cobos.csv')
