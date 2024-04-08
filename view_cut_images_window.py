de5ef115b6fddc54db8247121f824c10221e92cd
Tyler Wheeler <wheelert@steelhead.nscl.msu.edu>
2024-04-08 15:32:13 -0400

    Viewer now searches for PNGs, image file names are now listed below each image, the single image view enlarges the image, fixed the go_to function

--- view_cut_images_window.py
+++ view_cut_images_window.py
@@ -1,108 +1,132 @@
 import os
 import glob
-
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-
 from PIL import Image, ImageTk
-from tqdm import tqdm
 
 class ViewCutImagesWindow(tk.Toplevel):
-    def __init__(self, parent, run_data, selected_dir,**kwargs):
+    def __init__(self, parent, run_data, selected_dir, **kwargs):
         super().__init__(parent, **kwargs)
         self.run_data = run_data
         self.selected_dir = selected_dir
 
-        #load images
-        image_search_string = os.path.join(selected_dir, "*.jpg")
+        # Load images
+        image_search_string = os.path.join(selected_dir, "*.png")
         self.image_path_list = glob.glob(image_search_string)
 
-        #setup GUI
+        # Setup GUI components
         self.notebook = ttk.Notebook(self)
         self.back_button = ttk.Button(self, text='<<', command=self.back)
         self.next_button = ttk.Button(self, text='>>', command=self.next)
         self.go_to_entry = ttk.Entry(self)
         self.go_to_button = ttk.Button(self, text='Go to Image', command=self.go_to)
         
-        self.single_image_label = ttk.Label(self.notebook, anchor=tk.CENTER )
-        self.notebook.add(self.single_image_label, text='single')
+        # Setup single image frame with a frame to include the file name
+        self.single_image_frame = ttk.Frame(self.notebook)
+        self.single_image_label = ttk.Label(self.single_image_frame, anchor=tk.CENTER)
+        self.single_image_label.grid(row=0, column=0)
+        self.single_filename_label = tk.Label(self.single_image_frame, text="", font=('Helvetica', 14))
+        self.single_filename_label.grid(row=1, column=0)
+        self.notebook.add(self.single_image_frame, text='single')
 
+        # Setup grid for displaying multiple images
         self.grid_frame = ttk.Frame(self.notebook)
         self.grid_image_labels = []
+        self.grid_filename_labels = []  # Labels for the filenames in the grid
         for i in range(9):
-            self.grid_image_labels.append(ttk.Label(self.grid_frame))
-            self.grid_image_labels[i].grid(row=int(i/3), column=i%3)
+            image_label = ttk.Label(self.grid_frame)
+            self.grid_image_labels.append(image_label)
+            image_label.grid(row=int(i/3)*2, column=i%3)
+            
+            filename_label = tk.Label(self.grid_frame, text="", font=('Helvetica', 12))
+            self.grid_filename_labels.append(filename_label)
+            filename_label.grid(row=int(i/3)*2+1, column=i%3)
         self.notebook.add(self.grid_frame, text='3x3')
 
+        # Positioning the main GUI components
         self.back_button.grid(row=0, column=0)
         self.next_button.grid(row=0, column=3)
         self.go_to_entry.grid(row=1, column=1)
         self.go_to_button.grid(row=1, column=2)
         self.notebook.grid(row=2, column=0, columnspan=4)
 
-        self.current_index = 0 #track which image we're viewing
+        self.current_index = 0  # Track which image we're viewing
         self.change_index(self.current_index)
-        
+
+        # Bind the <ButtonRelease-1> event to handle tab changes
+        self.notebook.bind("<ButtonRelease-1>", self.on_tab_change)
+
+
+    def on_tab_change(self, event=None):
+        self.change_index(self.current_index)
+
+
     def change_index(self, index):
         self.current_index = index
-        #enable/disable forward and backwards buttons based on selected tab
-        if self.notebook.tab(self.notebook.select(), "text")=='single':
-            if self.current_index == 0:
-                self.back_button.state(["disabled"])
+        # Enable/disable forward and backward buttons
+        self.back_button.state(["!disabled" if index > 0 else "disabled"])
+        self.next_button.state(["!disabled" if index < len(self.image_path_list) - 1 else "disabled"])
+
+        single_image_size = (840, 680)  # New, larger size for the single image
+        grid_image_size = (420, 340)  # Existing size for grid images
+
+        if index < len(self.image_path_list):
+            # Open the current image
+            image_path = self.image_path_list[index]
+            image = Image.open(image_path)
+
+            # Adjust the image size based on the selected tab
+            if self.notebook.tab(self.notebook.select(), "text") == 'single':
+                # Resize while maintaining the aspect ratio
+                aspect_ratio = min(single_image_size[0] / image.width, single_image_size[1] / image.height)
+                new_size = (int(image.width * aspect_ratio), int(image.height * aspect_ratio))
+                image = image.resize(new_size, Image.Resampling.LANCZOS)
             else:
-                self.back_button.state(["!disabled"])
-            if self.current_index == (len(self.image_path_list) - 1):
-                self.next_button.state(["disabled"])
-            else:
-                self.next_button.state(["!disabled"])
-        else: #3x3 grid
-            if self.current_index == 0:
-                self.back_button.state(["disabled"])
-            else:
-                self.back_button.state(["!disabled"])
-            if self.current_index == (len(self.image_path_list) - 9):
-                self.next_button.state(["disabled"])
-            else:
-                self.next_button.state(["!disabled"])
-        #display single image
-        self.single_image = ImageTk.PhotoImage(
-            Image.open(self.image_path_list[self.current_index]))
-        self.single_image_label.configure(image=self.single_image)
-        #update image grid
+                image.thumbnail(grid_image_size, Image.Resampling.LANCZOS)
+
+            # Update the image display
+            self.single_image = ImageTk.PhotoImage(image)
+            self.single_image_label.configure(image=self.single_image)
+            file_name = os.path.basename(image_path)
+            self.single_filename_label.config(text=file_name)
+
+        # Update the 3x3 grid and filenames
         self.grid_images = []
         for i in range(9):
-            if self.current_index + i >= len(self.image_path_list):
-                break
-            im = Image.open(self.image_path_list[self.current_index + i])
-            im.thumbnail((420, 340), Image.Resampling.LANCZOS)
-            self.grid_images.append(ImageTk.PhotoImage(im))
-            self.grid_image_labels[i].configure(image=self.grid_images[i])
+            if index + i < len(self.image_path_list):
+                im = Image.open(self.image_path_list[index + i])
+                im.thumbnail(grid_image_size, Image.Resampling.LANCZOS)
+                photo_image = ImageTk.PhotoImage(im)
+                self.grid_images.append(photo_image)
+                self.grid_image_labels[i].configure(image=photo_image)
+                file_name = os.path.basename(self.image_path_list[index + i])
+                self.grid_filename_labels[i].config(text=file_name)
+            else:
+                self.grid_image_labels[i].configure(image='')
+                self.grid_filename_labels[i].config(text='')
+
 
     def next(self):
-        if self.notebook.tab(self.notebook.select(), "text")=='single':
-            new_index = self.current_index + 1
-        else: #3x3 grid
-            new_index = self.current_index + 9
-            if new_index > len(self.image_path_list):
-                new_index = len(self.image_path_list) - 1
+        new_index = self.current_index + 1 if self.notebook.tab(self.notebook.select(), "text") == 'single' else self.current_index + 9
+        new_index = min(new_index, len(self.image_path_list) - 1)
         self.change_index(new_index)
+
 
     def back(self):
-        if self.notebook.tab(self.notebook.select(), "text")=='single':
-            new_index = self.current_index - 1
-        else: #3x3 grid
-            new_index = self.current_index - 9
-            if new_index < 0:
-                new_index = 0
+        new_index = self.current_index - 1 if self.notebook.tab(self.notebook.select(), "text") == 'single' else self.current_index - 9
+        new_index = max(new_index, 0)
         self.change_index(new_index)
 
+
     def go_to(self):
-        event_num = int(self.go_to_entry.get())
-        uncut_index = self.run_data.get_index(event_num)
-        search_string = f'image_{uncut_index}.jpg'
-        for i, fname in enumerate(self.image_path_list):
-            if search_string in fname:
-                self.change_index(i)
-                return
-        messagebox.showwarning('Event not found!', 'Event not found!')
+        try:
+            event_num = int(self.go_to_entry.get())
+            search_string = f'image_{event_num}.png'
+            for i, fname in enumerate(self.image_path_list):
+                if search_string in fname:
+                    self.change_index(i)
+                    return
+            messagebox.showwarning('Event not found!', 'Event not found!')
+        except ValueError:
+            messagebox.showerror('Invalid Input', 'Please enter a valid number.')
