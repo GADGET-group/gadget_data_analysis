53713c12534fd2c48e21e9b2312acb4f24d390bd
Adams, Alexander <adamsa@frib.msu.edu>
2024-08-13 16:16:51 -0400

    added logic for testing for convergence

--- track_fit_test.py
+++ track_fit_test.py
@@ -189,7 +189,6 @@ h5file.plot_3d_traces(event_num, threshold=100)
 #do MCMC
 import emcee
 
-   
 class GaussianVar:
     def __init__(self, mu, sigma):
         self.mu, self.sigma  = mu, sigma
@@ -221,7 +220,7 @@ P_prior = GaussianVar(P_guess, P_guess*0.01)#assumes pressure transducer accurac
 x_real, y_real, z_real, e_real = h5file.get_xyze(event_number=event_num)
 xmin, xmax = np.min(x_real), np.max(x_real)
 ymin, ymax = np.min(y_real), np.max(y_real)
-zmin, zmax = np.min(z_real), np.max(z_real)
+zmin, zmax = 5, 400
 
 
 def log_priors(params):
@@ -231,17 +230,20 @@ def log_priors(params):
         return -np.inf
     if theta < 0 or theta >= np.pi:
         return -np.inf
-    if phi < 0 or phi > 2*np.pi:
+    if phi <= -np.pi or phi > np.pi:
         return -np.inf
     if charge_spread <= 0 or charge_spread > 10:
         return -np.inf
-    if shaping_width <=0 or shaping_width > 15:
+    if shaping_width <=0 or shaping_width > 20:
         return -np.inf
     #gaussian priors
     return E_prior.log_likelihood(E) + P_prior.log_likelihood(P) + adc_scale_prior.log_likelihood(adc_scale)
 
 def log_posterior(params):
-    return log_priors(params) + log_likelihood_mcmc(params)
+    to_return = log_priors(params) + log_likelihood_mcmc(params)
+    print('log posterior: %e'%to_return)
+    return to_return
+    
 
 #use previous optimization for start pos
 #E=6.496048 MeV, (x,y,z)=(-12.865501, 12.899337, 50.000000) mm, theta = 86.718415 deg, phi=-29.475943 deg, cs=4.179261 mm, shaping=10.126000, P=1157.000000 torr,  LL=7.633177e+06
@@ -250,5 +252,70 @@ nwalkers = 50
 ndim = 10
 init_walker_pos =  [np.array(start_pos) + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
 
+backend_file = "run368_event%d_samples.h5"%(event_num)
+backend = emcee.backends.HDFBackend(backend_file)
+backend.reset(nwalkers, ndim)
+
 sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior)
-sampler.run_mcmc(init_walker_pos, 500, progress=True)
+max_n = 100000
+# We'll track how the average autocorrelation time estimate changes
+index = 0
+autocorr = np.empty(max_n)
+# This will be useful to testing convergence
+old_tau = np.inf
+# Now we'll sample for up to max_n steps
+for sample in sampler.sample(init_walker_pos, iterations=max_n, progress=True):
+    # Only check convergence every 10 steps
+    if sampler.iteration % 100:
+        continue
+
+    # Compute the autocorrelation time so far
+    # Using tol=0 means that we'll always get an estimate even
+    # if it isn't trustworthy
+    tau = sampler.get_autocorr_time(tol=0)
+    autocorr[index] = np.mean(tau)
+    index += 1
+    print('%f < tau < %f on iteration %d'%(np.min(tau), np.max(tau), sampler.iteration ))
+
+    # Check convergence
+    converged = np.all(tau * 100 < sampler.iteration)
+    converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
+    if converged:
+        break
+    old_tau = tau
+
+
+#sampler.run_mcmc(init_walker_pos, 100, progress=True)
+
+samples = sampler.get_chain()
+labels = ['E', 'x','y','z','theta', 'phi', 'charge_spread', 'shaping_width', 'P', 'adc_scale', 'logf']
+fig, axes = plt.subplots(len(labels), figsize=(10, 7), sharex=True)#len(labels)
+for i in range(len(labels)):
+    ax = axes[i]
+    ax.plot(samples[:, :, i], "k", alpha=0.3)
+    ax.set_xlim(0, len(samples))
+    ax.set_ylabel(labels[i])
+    ax.yaxis.set_label_coords(-0.1, 0.5)
+
+axes[-1].set_xlabel("step number")
+
+import corner
+tau = sampler.get_autocorr_time()
+burnin = int(2 * np.max(tau))
+thin = int(0.5 * np.min(tau))
+samples = sampler.get_chain(discard=burnin, flat=True, thin=thin)
+log_prob_samples = sampler.get_log_prob(discard=burnin, flat=True, thin=thin)
+log_prior_samples = sampler.get_blobs(discard=burnin, flat=True, thin=thin)
+
+print("burn-in: {0}".format(burnin))
+print("thin: {0}".format(thin))
+print("flat chain shape: {0}".format(samples.shape))
+print("flat log prob shape: {0}".format(log_prob_samples.shape))
+print("flat log prior shape: {0}".format(log_prior_samples.shape))
+
+all_samples = np.concatenate(
+    (samples, log_prob_samples[:, None], log_prior_samples[:, None]), axis=1
+)
+
+corner.corner(all_samples, labels=labels)
+plt.show()
