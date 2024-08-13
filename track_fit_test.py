6df9b60ae419901a80fad714dc7e37a87fd704a4
Adams, Alexander <adamsa@frib.msu.edu>
2024-08-13 14:12:24 -0400

    added mcmc to track_fit_test.py

--- track_fit_test.py
+++ track_fit_test.py
@@ -1,7 +1,7 @@
 import time
 
-import os
-os.environ['OPENBLAS_NUM_THREADS'] = '3'
+#import os
+#os.environ['OPENBLAS_NUM_THREADS'] = '3'
 import numpy as np
 
 import matplotlib.pylab as plt
@@ -21,7 +21,9 @@ if event_num == 5:
     phi_guess = np.radians(-30)
     P_guess = 1157
 
-adc_scale = 376646/6.288 #counts/MeV, from fitting events with range 40-43 in run 0368 with p10_default
+adc_scale_mu = 371902/6.288 #counts/MeV, from fitting events with range 40-43 in run 0368 with p10_default
+adc_scale_sigma = 1401/6.288 #uncertainty in peak postiion from chi^2 fit
+detector_E_sigma = 19506/adc_scale_mu #sigma for above fit
 
 #use theoretical zscale
 clock_freq = 50e6 #Hz
@@ -50,7 +52,7 @@ get_gas_density = lambda P: rho0*(P/760)*(300./T)
 trace_sim = SingleParticleEvent.SingleParticleEvent(get_gas_density(P_guess), 'alpha')
 trace_sim.shaping_width = shaping_width
 trace_sim.zscale = zscale
-trace_sim.counts_per_MeV = adc_scale
+trace_sim.counts_per_MeV = adc_scale_mu
 
 trace_sim.initial_energy = E_guess
 trace_sim.phi = phi_guess
@@ -141,9 +143,11 @@ def show_simulated_3d_data(mode,  threshold = 0.0001): #show plots of initial gu
     ax.axes.set_ylim3d(bottom=-100, top=100) 
     ax.axes.set_zlim3d(bottom=0, top=200)
 
-def neg_log_likelihood(params):
-    E, x, y, z, theta, phi, charge_spread, shaping_width, P = params
-    #P = P_guess
+#do initial minimization before starting MCMC
+def neg_log_likelihood_init_min(params):
+    E, x, y, theta, phi, charge_spread, shaping_width = params
+    P = P_guess
+    z = init_position_guess[2]
     trace_sim.load_srim_table('alpha', get_gas_density(P))
     trace_sim.initial_energy = E
     trace_sim.initial_point = (x,y,z)
@@ -160,43 +164,91 @@ def neg_log_likelihood(params):
 
 fit_start_time = time.time()
 
-Ebounds = (5,7)
-x_real, y_real, z_real, e_real = h5file.get_xyze(event_number=event_num)
-x_bounds = (np.min(x_real), np.max(x_real))
-y_bounds = (np.min(y_real), np.max(y_real))
-z_bounds = (10,400)
-theta_bounds = (0, np.radians(180))
-phi_bounds = (0., 2*np.pi)
-cs_bounds = (0,10)#mm
-cpe_bounds = (0.8*adc_scale, 1.2*adc_scale)
-initial_guess = (E_guess, *init_position_guess, theta_guess, phi_guess, charge_spreading_guess, shaping_width, P_guess)
-#opt_results = opt.shgo(func=neg_log_likelihood, bounds=[Ebounds, x_bounds, y_bounds, z_bounds, theta_bounds, phi_bounds, cs_bounds, cpe_bounds])
-#print(opt_results)
-#xopt = opt.fmin_powell(func=neg_log_likelihood, x0=(E_guess, *init_position_guess, theta_guess, phi_guess, charge_spreading, trace_sim.shaping_width), ftol=1000)
-res = opt.minimize(fun=neg_log_likelihood, x0=initial_guess, method="Powell")
-#res = opt.basinhopping(func=neg_log_likelihood, x0=(E_guess, *init_position_guess, theta_guess, phi_guess, 0))
-#res = opt.minimize(fun=neg_log_likelihood, x0=(E_guess, *init_position_guess, theta_guess, phi_guess, charge_spreading, trace_sim.shaping_width), options={'adaptive':True})
-#res = opt.minimize(fun=neg_log_likelihood, method='BFGS', x0=(E_guess, *init_position_guess, theta_guess, phi_guess, charge_spreading, trace_sim.counts_per_MeV/1e4))
-#res = opt.differential_evolution(func=neg_log_likelihood, bounds=[Ebounds, x_bounds, y_bounds, z_bounds, theta_bounds, phi_bounds, cs_bounds, cpe_bounds], workers=1)
-#print(xopt)
+initial_guess = (E_guess, *init_position_guess[0:2], theta_guess, phi_guess, charge_spreading_guess, shaping_width)
+
+#get log likilihood within 0.1%
+'''res = opt.minimize(fun=neg_log_likelihood_init_min, x0=initial_guess, method="Powell", options={'disp':True, 'ftol':0.01, 'xtol':1})
+
 
 print(res)
-neg_log_likelihood(res.x)
+neg_log_likelihood_init_min(res.x)
+
+
 print('total fit time: %f s'%(time.time() - fit_start_time))
 
 plot_traces(trace_sim.traces_to_fit, 'clipped real traces')
 plot_traces(trace_sim.aligned_sim_traces, 'simulated traces')
 plot_residuals()
 plot_residuals_3d()
-#best fit from differential evolution: E=6.034191 MeV, (x,y,z)=(26.532488, 16.171300, 39.979532) mm, theta = 149.786768deg, phi=79.963981 deg, cs=5.403833 mm, LL=6.807297e+06
-#typical for direct: E=6.408290 MeV, (x,y,z)=(-19.338566, 13.018618, 100.294812) mm, theta = 85.538278 deg, phi=-24.128679 deg, cs=6.449649 mm, cpe=5.947572e+04 LL=4.876868e+07
 
 show_simulated_3d_data(mode='aligned', threshold=100)
 h5file.plot_3d_traces(event_num, threshold=100)
+'''
 
 
 #do MCMC
 import emcee
 
+   
+class GaussianVar:
+    def __init__(self, mu, sigma):
+        self.mu, self.sigma  = mu, sigma
+
+    def log_likelihood(self, val):
+        return -np.log(np.sqrt(2*np.pi*self.sigma**2)) - (val - self.mu)**2/2/self.sigma
+
+def log_likelihood_mcmc(params):
+    E, x,y,z,theta, phi, charge_spread, shaping_width, P, adc_scale = params
+    trace_sim.load_srim_table('alpha', get_gas_density(P))
+    trace_sim.initial_energy = E
+    trace_sim.initial_point = (x,y,z)
+    trace_sim.theta = theta
+    trace_sim.phi = phi
+    trace_sim.charge_spreading_sigma = charge_spread
+    trace_sim.shaping_width = shaping_width
+    trace_sim.counts_per_MeV = adc_scale
+    trace_sim.simulate_event()
+    trace_sim.align_pad_traces()
+    to_return = trace_sim.log_likelihood()
+    print('E=%f MeV, (x,y,z)=(%f, %f, %f) mm, theta = %f deg, phi=%f deg, cs=%f mm, shaping=%f, P=%f torr, adc_scale=%f, LL=%e'%(E, x,y,z,np.degrees(theta), np.degrees(phi), charge_spread, shaping_width, P, adc_scale, to_return))
+    return to_return
+
+adc_scale_prior = GaussianVar(adc_scale_mu, adc_scale_sigma)
+max_veto_pad_counts, dxy, dz, measured_counts, angle, pads_railed = h5file.process_event(event_num)
+E_from_ic = measured_counts/adc_scale_mu
+E_prior = GaussianVar(E_from_ic, detector_E_sigma)
+P_prior = GaussianVar(P_guess, P_guess*0.01)#assumes pressure transducer accuracy of 1%, should check what this really should be
+x_real, y_real, z_real, e_real = h5file.get_xyze(event_number=event_num)
+xmin, xmax = np.min(x_real), np.max(x_real)
+ymin, ymax = np.min(y_real), np.max(y_real)
+zmin, zmax = np.min(z_real), np.max(z_real)
+
+
 def log_priors(params):
-    
+    E, x,y,z,theta, phi, charge_spread, shaping_width, P, adc_scale = params
+    #uniform priors
+    if x < xmin or x > xmax or y < ymin or y > ymax or z < zmin or z>zmax:
+        return -np.inf
+    if theta < 0 or theta >= np.pi:
+        return -np.inf
+    if phi < 0 or phi > 2*np.pi:
+        return -np.inf
+    if charge_spread <= 0 or charge_spread > 10:
+        return -np.inf
+    if shaping_width <=0 or shaping_width > 15:
+        return -np.inf
+    #gaussian priors
+    return E_prior.log_likelihood(E) + P_prior.log_likelihood(P) + adc_scale_prior.log_likelihood(adc_scale)
+
+def log_posterior(params):
+    return log_priors(params) + log_likelihood_mcmc(params)
+
+#use previous optimization for start pos
+#E=6.496048 MeV, (x,y,z)=(-12.865501, 12.899337, 50.000000) mm, theta = 86.718415 deg, phi=-29.475943 deg, cs=4.179261 mm, shaping=10.126000, P=1157.000000 torr,  LL=7.633177e+06
+start_pos = [6.496048, -12.8865501,12.89937,50,np.radians(86.718415), np.radians(-29.475943), 4.179261,10.126,P_guess, adc_scale_mu]
+nwalkers = 50
+ndim = 10
+init_walker_pos =  [np.array(start_pos) + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
+
+sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior)
+sampler.run_mcmc(init_walker_pos, 500, progress=True)
