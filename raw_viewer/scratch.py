4541cac0b485e53b91b199787dda892223e88c16
Alexander Adams <adamsa@frib.msu.edu>
2025-02-20 11:41:32 -0500

    reduced lower end of range cut, and removed duplicate run number

--- raw_viewer/scratch.py
+++ raw_viewer/scratch.py
@@ -1,12 +1,17 @@
 import os
 
 import numpy as np
+import cupy as cp
 import matplotlib.pylab as plt
 from tqdm import tqdm
 
 from track_fitting import build_sim
 
 run_number = 124
+if run_number == 124:
+    processed_directory = '/egr/research-tpc/shared/Run_Data/run_0124_raw_viewer/run_0124smart'
+elif run_number == 212:
+    processed_directory = '/egr/research-tpc/shared/Run_Data/run_0212_raw_viewer/run_0212smart'
 
 h5file = build_sim.get_rawh5_object('e21072', run_number)
 zscale = h5file.zscale
@@ -22,22 +27,27 @@ dzs = dts*zscale
 ranges = np.sqrt(dzs**2 + dxys**2)
 angles = np.arctan2(dzs, dxys)
 
+std_plots = False
+
 if run_number == 124:
-    processed_directory = '/egr/research-tpc/shared/Run_Data/run_0124_raw_viewer/run_0124smart_20_length_thresh'
     #1500 keV protons
-    if False:
-        selected_event_mask = (ranges > 35) & (ranges < 65) & (counts > 1.64e5) & (counts < 2.15e5)
-        range_histogram_bins = np.linspace(35,65,10)
+    if True:
+        selected_event_mask = (ranges > 31) & (ranges < 65) & (counts > 1.64e5) & (counts < 2.15e5)
+        range_histogram_bins = np.linspace(35,65,20)
     #4.4 MeV alpha from cathode
     if False:
         selected_event_mask = (ranges > 25) & (ranges < 50) & (counts > 4.5e5) & (counts < 5.8e5)
         range_histogram_bins = np.linspace(25,45,10)
     #4.4 MeV alpha NOT from cathode
-    if True:
+    if False:
         selected_event_mask = (ranges > 30) & (ranges < 55) & (counts > 5.9e5) & (counts < 7e5)
         range_histogram_bins = np.linspace(30,55,10)
 elif run_number == 212:
-    processed_directory = '/egr/research-tpc/shared/Run_Data/run_0212_raw_viewer/run_0212smart'
+    #1500 keV protons
+    if True:
+        selected_event_mask = (ranges > 31) & (ranges < 70) & (counts > 3e5) & (counts < 3.6e5)
+        range_histogram_bins = np.linspace(35,65,20)
+
 
 first_event, last_event = h5file.get_event_num_bounds()
 selected_events =  np.nonzero(selected_event_mask)[0] + first_event
@@ -73,22 +83,24 @@ for evt in tqdm(selected_events):
     
     dir = dir/np.linalg.norm(dir)
     width, width_xy, width_z = 0,0,0 #width will be the standard deviation of the distance of charge from principle axis)
-    for x,y,z,e in zip(xs, ys, zs, es):
-        distance_from_axis = np.linalg.norm(np.cross(np.array([x,y,z]) - center, dir))
-        center_xy = np.array([center[0], center[1], 1])
-        distance_xy = np.linalg.norm(np.cross(np.array([x,y,0]) - center, dir))
-        width += distance_from_axis**2*e
-        width_xy += distance_xy**2*3
-        
-        distance_z = z - (center + (np.linalg.norm(np.array([x,y,z]) - center)*dir))[2]
-        width_z += distance_z**2*e
-    widths.append(np.sqrt(width/np.sum(es)))
-    widths_xy.append(np.sqrt(width_xy/np.sum(es)))
-    widths_z.append(np.sqrt(width_z/np.sum(es)))
+    if std_plots:
+        for x,y,z,e in zip(xs, ys, zs, es):
+            distance_from_axis = np.linalg.norm(np.cross(np.array([x,y,z]) - center, dir))
+            center_xy = np.array([center[0], center[1], 1])
+            distance_xy = np.linalg.norm(np.cross(np.array([x,y,0]) - center, dir))
+            width += distance_from_axis**2*e
+            width_xy += distance_xy**2*3
+            
+            distance_z = z - (center + (np.linalg.norm(np.array([x,y,z]) - center)*dir))[2]
+            width_z += distance_z**2*e
+        widths.append(np.sqrt(width/np.sum(es)))
+        widths_xy.append(np.sqrt(width_xy/np.sum(es)))
+        widths_z.append(np.sqrt(width_z/np.sum(es)))
 
-widths = np.array(widths)
-widths_xy = np.array(widths_xy)
-widths_z = np.array(widths_z)
+if std_plots:
+    widths = np.array(widths)
+    widths_xy = np.array(widths_xy)
+    widths_z = np.array(widths_z)
 thetas = np.array(thetas)
 phis = np.array(phis)
 track_centers = np.array(track_centers)
@@ -101,25 +113,26 @@ theta_max = np.radians(90)
 theta_mask = (thetas>=theta_min) & (thetas <= theta_max)
 plt.figure()
 rs = np.sqrt(track_centers[:,0]**2 + track_centers[:, 1]**2)
-plt.scatter(rs[theta_mask], ranges[theta_mask], marker='.')
+plt.hist2d(rs[theta_mask], ranges[theta_mask], bins=20)
 plt.xlabel('track centroid distance from beam axis (mm)')
 plt.ylabel('track length (mm)')
 
-#make histogram of range vs width
-plt.figure()
-plt.scatter(widths[theta_mask], ranges[theta_mask], marker='.')
-plt.xlabel('track width (mm)')
-plt.ylabel('track length (mm)')
+if std_plots:
+    #make histogram of range vs width
+    plt.figure()
+    plt.scatter(widths[theta_mask], ranges[theta_mask], marker='.')
+    plt.xlabel('track width (mm)')
+    plt.ylabel('track length (mm)')
 
-plt.figure()
-plt.scatter(widths_xy[theta_mask], ranges[theta_mask], marker='.')
-plt.xlabel('track width xy (mm)')
-plt.ylabel('track length (mm)')
+    plt.figure()
+    plt.scatter(widths_xy[theta_mask], ranges[theta_mask], marker='.')
+    plt.xlabel('track width xy (mm)')
+    plt.ylabel('track length (mm)')
 
-plt.figure()
-plt.scatter(widths_z[theta_mask], ranges[theta_mask], marker='.')
-plt.xlabel('track width z (mm)')
-plt.ylabel('track length (mm)')
+    plt.figure()
+    plt.scatter(widths_z[theta_mask], ranges[theta_mask], marker='.')
+    plt.xlabel('track width z (mm)')
+    plt.ylabel('track length (mm)')
 
 
 plt.figure()
