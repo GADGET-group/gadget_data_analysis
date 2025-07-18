840e950468136a6e6112594619c16ec5d3ea0c8e
Alexander Adams <adamsa@frib.msu.edu>
2025-07-18 13:56:41 -0400

    select cupy device manually

--- raw_viewer/pad_gain_match/toy_model.py
+++ raw_viewer/pad_gain_match/toy_model.py
@@ -6,7 +6,7 @@ import cupy as cp
 
 #build images
 dim = 40
-num_events = 10000
+num_events = 100000
 threshold = 1
 counts_per_event=1000
 sigma_min, sigma_max = 3,5
@@ -29,19 +29,20 @@ plt.imshow(image)
 plt.colorbar()
 plt.show()
 
-pad_images_cp = cp.array(pad_images)
-observed_counts_per_event = cp.sum(pad_images_cp)/num_events
-def obj_func(x):
-    x = cp.array(x)
-    gains = cp.reshape(x, (dim, dim))
-    adc_counts_in_each_event = cp.einsum('ikj,kj', pad_images_cp, gains)/observed_counts_per_event
-    return cp.asnumpy(cp.sqrt(cp.sum((adc_counts_in_each_event - 1)**2)/len(adc_counts_in_each_event))*2.355)
+with cp.cuda.Device(1):
+    pad_images_cp = cp.array(pad_images)
+    observed_counts_per_event = cp.sum(pad_images_cp)/num_events
+    def obj_func(x):
+        x = cp.array(x)
+        gains = cp.reshape(x, (dim, dim))
+        adc_counts_in_each_event = cp.einsum('ikj,kj', pad_images_cp, gains)/observed_counts_per_event
+        return cp.asnumpy(cp.sqrt(cp.sum((adc_counts_in_each_event - 1)**2)/len(adc_counts_in_each_event))*2.355)
 
-def callback(x):
-    print(x, obj_func(x))
-    print(np.mean(x), np.std(x), np.min(x), np.max(x))
+    def callback(x):
+        print(x, obj_func(x))
+        print(np.mean(x), np.std(x), np.min(x), np.max(x))
 
-res = opt.minimize(obj_func, np.ones(dim*dim), callback = callback)
+    res = opt.minimize(obj_func, np.ones(dim*dim), callback = callback)
 gains = np.reshape(res.x, (dim, dim))
 print(res)
 
@@ -64,4 +65,10 @@ plt.figure()
 plt.title('gain residuals (fit - truth)')
 plt.imshow(gains - true_gains)
 plt.colorbar()
+
+bins = np.linspace(np.min([gains, true_gains]), np.max([gains, true_gains]), 300)
+plt.figure()
+plt.hist(true_gains.flatten(), bins, label='true gains')
+plt.hist(gains.flatten(), bins, label='fit gains', alpha=0.5)
+plt.legend()
 plt.show()
