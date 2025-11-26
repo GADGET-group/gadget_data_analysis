f650e2f1f001187cb27faffe2e608caf4993f0bd
Alexander Adams <adamsa@frib.msu.edu>
2025-11-26 16:35:25 -0500

    added exp_env.py

---
+++ exp_env.py
@@ -0,0 +1,21 @@
+import sys
+import importlib
+
+def import_or_reload(module_name):
+    if module_name in sys.modules:
+        module_obj = sys.modules[module_name]
+        importlib.reload(module_obj)
+    else:
+        importlib.import_module(module_name)
+
+def rve():
+    import_or_reload('raw_viewer.plots.rve')
+
+def rates():
+    import_or_reload('raw_viewer.plots.rates')
+    
+def rate_summary():
+    import_or_reload('raw_viewer.plots.rate_summary')
+
+def xy_centering():
+    import_or_reload('raw_viewer.plots.xy_centering')
