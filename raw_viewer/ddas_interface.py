5732a3bb32f9c772e289bd94a0c00048ad308441
Alexander Adams <adamsa@frib.msu.edu>
2025-12-26 11:02:59 -0500

    updates to ddas_interface

--- raw_viewer/ddas_interface.py
+++ raw_viewer/ddas_interface.py
@@ -1,6 +1,18 @@
 import ROOT
 import numpy as np
 
+class CH_MAP:
+    MESH_PRE_AMP = 150
+    GET_TRIG_ACCEPTED = 151
+    SCA_LOGIC = 152
+    VETO_LOGIC = 153
+    #beam off is signal from ARIS used to switch GG
+    BEAM_ON = 154
+    BEAM_OFF = 155
+    #chopper is actual control of accelerator
+    CHOPPER_ON = 156
+    CHOPPER_OFF = 157
+
 def get_root_file_path(experiment, run):
     base_path = f'/egr/research-tpc/shared/proc_runs/{experiment}/ddas/'
     file_name = 'run-%04d_gadget.root'%run
@@ -9,7 +21,10 @@ def get_root_file_path(experiment, run):
 def extract_data(experiment, run):
     file = ROOT.TFile(get_root_file_path(experiment, run), "READ")
     tree = file.Get("tree")
-    energies, times, multiplicities = np.zeros(160), np.zeros(160), np.zeros(160)
+    energies, times, multiplicities = np.zeros(160, dtype=np.int32), np.zeros(160), np.zeros(160,dtype=np.int32)
+    tree.SetBranchAddress("energies", energies)
+    tree.SetBranchAddress("times", times)
+    tree.SetBranchAddress("multiplicity", multiplicities)
     es, ts, ms = [], [], []
     for i in range(tree.GetEntries()):
         tree.GetEntry(i)
@@ -17,3 +32,22 @@ def extract_data(experiment, run):
         ts.append(np.copy(times))
         ms.append(np.copy(multiplicities))
     return np.array(es), np.array(ts), np.array(ms)
+
+
+def get_time_since_beam_off(es, ts, ms):
+    '''
+    Get time since beam as turned off for each accepted trigger
+    ''' 
+    beam_off_times = ts[:, CH_MAP.CHOPPER_OFF]
+    beam_off_times = beam_off_times[beam_off_times >= 0]
+    event_times = ts[:, CH_MAP.GET_TRIG_ACCEPTED]
+    event_times = event_times[event_times >= 0]
+    
+    to_return = np.zeros(len(event_times))
+    i, j = 0,0
+    while i < len(event_times):
+        while (j < len(beam_off_times) - 1) and (beam_off_times[j+1] < event_times[i]):
+            j += 1
+        to_return[i] = event_times[i] - beam_off_times[j]
+        i += 1
+    return to_return
